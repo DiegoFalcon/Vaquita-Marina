bacbbbd9ee97f64ad88a32333102db62a30310ca
itzelbareno <itzelbareno@gmail.com>
2015-10-11 13:30:46 -0700

    Clase Variable
    Se agregó clase Variable

---
+++ Variable.java
@@ -0,0 +1,18 @@
+package compilador;
+
+public class Variable {
+	public String name;
+	public String value;
+	public String type;
+	
+	Variable(){
+		name="";
+		value="";
+		type="";
+	}
+	Variable(String newName, String newValue, String newType){
+		name=newName;
+		value=newValue;
+		type=newType;
+	}
+}
